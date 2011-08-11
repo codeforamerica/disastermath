94a4083175988c7714e3926ba5911864a8aaf6a3
Bailey Smith <bailey@codeforamerica.org>
2011-08-11 10:45:46 -0700

    Add Katrina Protovis

---
+++ graphs/js/data.js
@@ -0,0 +1,24 @@
+var data =[
+	{Loc:"Sumner, Warren counties",Year:2006,Name:"None",Severity:6.7,Deaths:12,Affected:625,Damages:1600},
+	{Loc:"Galveston, Brazoria, Harr",Year:2008,Name:"Ike",Severity:6.7,Deaths:82,Affected:200000,Damages:30000},
+	{Loc:"Martin, Palm Beach",Year:2004,Name:"Frances",Severity:6.5,Deaths:47,Affected:5000000,Damages:11000},
+	{Loc:"Mobile, Bayou La Batre, D",Year:2005,Name:"Katrina",Severity:6.3,Deaths:1833,Affected:500000,Damages:125000},
+	{Loc:"Florida, Georgia, Alabama",Year:2008,Name:"Fay",Severity:6.2,Deaths:12,Affected:400,Damages:180},
+	{Loc:"North Carolina, Maryland,",Year:2003,Name:"Isabel",Severity:6.1,Deaths:16,Affected:225000,Damages:3370},
+	{Loc:"Appalachia, Eastern Seabo",Year:2003,Name:"None",Severity:6.1,Deaths:8,Affected:1125,Damages:null},
+	{Loc:"Illinois, Indiana, Iowa",Year:2004,Name:"Gustav",Severity:5.8,Deaths:4,Affected:337,Damages:1100},
+	{Loc:"Arkansas, Louisiana,",Year:2004,Name:"None",Severity:5.7,Deaths:5,Affected:1749,Damages:300},
+	{Loc:"Louisiana, Mississippi, Texas",Year:2008,Name:"Wilma",Severity:5.7,Deaths:43,Affected:2100000,Damages:7000},
+	{Loc:"Tennessee, Illinois, Arkansas",Year:2006,Name:"Rita",Severity:5.6,Deaths:28,Affected:3636,Damages:600},
+	{Loc:"Tennessee, Kentucky",Year:2002,Name:"Allison",Severity:5.6,Deaths:6,Affected:754,Damages:200},
+	{Loc:"Florida Keys, Naples",Year:2005,Name:"None",Severity:5.5,Deaths:4,Affected:30000,Damages:14300},
+	{Loc:"Louisiana, Texas, Mississippi",Year:2005,Name:"Isidore",Severity:5.5,Deaths:10,Affected:300000,Damages:16000},
+	{Loc:"Texas, Louisiana, Florida",Year:2001,Name:"None",Severity:5.4,Deaths:41,Affected:172000,Damages:6000},
+	{Loc:"Washington, Oregon states",Year:2007,Name:"None",Severity:5.3,Deaths:17,Affected:1128,Damages:1000},
+	{Loc:"Alabama, Delaware, Kentucky",Year:2003,Name:"None",Severity:5.2,Deaths:44,Affected:120,Damages:200},
+	{Loc:"Louisiana, Mississippi, A",Year:2002,Name:"None",Severity:5.1,Deaths:1,Affected:13200,Damages:300},
+	{Loc:"Missouri, Arkansas, Illinois",Year:2006,Name:"None",Severity:4.9,Deaths:10,Affected:42,Damages:1200},
+	{Loc:"Fort Worth Texas",Year:2000,Name:"None",Severity:4.9,Deaths:5,Affected:3300,Damages:450},
+	{Loc:"Tennessee, Kentucky",Year:2001,Name:"None",Severity:4.6,Deaths:11,Affected:200,Damages:null}
+];
+

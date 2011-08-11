23f34a8e61c2b699b4c4097d7c39e6ee83bba272
Linh Vuong <mlvuong@gmail.com>
2011-08-11 10:50:40 -0700

    Adding Haiti
    Needs Region on Y axis

---
+++ graphs/js/SEATsunami.js
@@ -0,0 +1,8 @@
+var data = [
+{Country:"Myanmar",Year:2004,Severity:9.1,Deaths:71,Affected:15700,Damages:500,Development:1},
+{Country:"Indonesia",Year:2004,Severity:9.1,Deaths:165708,Affected:532898,Damages:4451.6,Development:2},
+{Country:"Thailand",Year:2004,Severity:9.1,Deaths:8345,Affected:67007,Damages:1000,Development:3},
+{Country:"Sri Lanka",Year:2004,Severity:9.1,Deaths:35399,Affected:1019306,Damages:1316.5,Development:2},
+{Country:"Maldives",Year:2004,Severity:9.1,Deaths:102,Affected:27214,Damages:470.1,Development:3},
+{Country:"India",Year:2004,Severity:9.1,Deaths:16389,Affected:654512,Damages:1022.8,Development:2},
+{Country:"Malaysia",Year:2004,Severity:9.1,Deaths:80,Affected:5063,Damages:500,Development:4}];
